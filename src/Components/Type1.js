f1e8f18ec848d6834819521918883450a4a06b8e
Kritika Choudhary <88735718+Kritika0804@users.noreply.github.com>
2023-01-29 19:32:07 +0530

    type component changed

--- src/Components/Type1.js
+++ src/Components/Type1.js
@@ -4,13 +4,21 @@ function Type1({ question }) {
   return (
     <div className='type1'>
         <div className='ques-no'>Question 1</div>
-        <div className='ques-name'>MCQ-One-Answer</div>
-        <div className='option'>
+        <div className='ques-name'>{question.question_text}</div>
+        {/* <div className='option'>
           <input type="radio" name="opt" value="opt-1"/>opt-1<br/>
           <input type="radio" name="opt" value="opt-2" checked/>opt-2<br/>
           <input type="radio" name="opt" value="opt-3"/>opt-3<br/>
           <input type="radio" name="opt" value="opt-4"/>opt-4<br/>
-        </div>
+        </div> */}
+        <div>
+            {question.choices.map((choice) => (
+              <div key={choice.id}>
+                <input type="radio" name={question.id} value={choice.id} />
+                <label>{choice.choice_text}</label>
+              </div>
+            ))}
+          </div>
     </div>
   )
 }
